bed281cba8eff0838b185542546c2b8deccdd109
sujeong <sj980204@gmail.com>
2024-02-09 00:00:40 +0900

    멤버변수와 지역변수의 초기화 멤버변수 - 객체 생성시 자동 초기화 지역변수 - 반드시 수동으로 초기화

---
+++ src/ref/InitDataMain.java
@@ -0,0 +1,11 @@
+package ref;
+
+public class InitDataMain {
+    public static void main(String[] args) {
+        InitData data = new InitData();
+        System.out.println("value1 = " + data.value);
+        System.out.println("value2 = " + data.value2);
+        int a;
+        //System.out.println(a); // 초기화 안해서 에러남
+    }
+}

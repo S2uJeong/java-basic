9054e714e8cddac1840461945b9a16da2993b910
sujeong <sj980204@gmail.com>
2024-02-22 07:58:22 +0900

    패키지 - import 안 쓰고 다른 위치의 클래스의 인스턴스 생성

---
+++ src/pack/PackageMain1.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        pack.a.User user = new pack.a.User();  // 다른 위치에 존재한 클래스의 인스턴스를 참조할 때
+    }
+}

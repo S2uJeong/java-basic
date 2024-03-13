69e24db18f16560696afc2191b21bf3699239bfe
sujeong <sj980204@gmail.com>
2024-03-13 20:13:27 +0900

    역할과 구현 분리 - 비즈니스 추가 시 수정할 게 많은 기존코드

---
+++ src/poly/car0/K3Car.java
@@ -0,0 +1,14 @@
+package poly.car0;
+
+public class K3Car {
+    public void startEngine() {
+        System.out.println("KrCar.startEngine");
+    }
+    public void offEngine() {
+        System.out.println("KrCar.offEngine");
+    }
+    public void pressAccelerator() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+
+}
